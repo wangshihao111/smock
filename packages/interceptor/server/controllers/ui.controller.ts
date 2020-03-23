6ac0a591f5c6970ccff4ab7de03ffc1f5b5904a4
王世豪 <shihao.wang@hand-china.com>
2020-03-23 20:16:06 +0800

    feat: 新增接口编辑接口

--- packages/interceptor/server/controllers/ui.controller.ts
+++ packages/interceptor/server/controllers/ui.controller.ts
@@ -1,5 +1,8 @@
+import { FileUtil } from './../utils/file-util';
+import { RequestUtil } from './../utils/request-util';
 import { DbUtil } from './../utils/db-util';
 import { Application, Request, Response } from 'express';
+import prettier from 'prettier';
 
 const baseUrl = '/__api';
 
@@ -23,18 +26,82 @@ export class UIController {
     res.status(200);
     res.send({
       code: 0,
-      message: '更新成功'
+      message: '更新成功',
+      list: body,
     });
   }
 
-  public getIntercept(req: Request, res: Response) {
+  private getIntercept(req: Request, res: Response) {
     const list = DbUtil.get('interceptList') || [];
     res.status(200);
     res.send(list);
   }
 
+  private deleteHistory(req: Request, res: Response) {
+    const api = req.body.path as string;
+    try {
+      FileUtil.deleteOneLog(api);
+      res.status(200);
+      res.send({
+        code: 0,
+        message: 'success'
+      })
+    } catch(e) {
+      console.log(e);
+      res.status(200);
+      res.send({
+        code: -1,
+        message: 'failed'
+      })
+    }
+  }
+
+  private getApiDetail(req: Request, res: Response) {
+    const { query } = req;
+    const { api } = query as { api: string };
+    try {
+      const json = FileUtil.getOneHistory(api) as any;
+      const data: any = {}
+      for (const key in json) {
+        const value = json[key];
+        data[key] = {
+          request: prettier.format(JSON.stringify(value.request), { parser: 'json' }),
+          response: {
+            ...(value.response || {}),
+            data: prettier.format(JSON.stringify(value.response?.data || {}), {
+              parser: 'json'
+            })
+          }
+        }
+      }
+      res.send(data);
+    } catch (e) {
+      res.status(500);
+      res.send();
+    }
+  }
+
+  private apiSave(req: Request, res: Response) {
+    const { body } = req;
+    FileUtil.updateRequestLog(body).then(() => {
+      res.status(200);
+      res.send({
+        code: 0,
+        message: 'success'
+      })
+    }).catch(e => {
+      console.log(e)
+      res.status(500);
+      res.send();
+    })
+  }
+
   public run(): void {
     this.app.get(`${baseUrl}/tree`, this.getApiTree)
     this.app.post(`${baseUrl}/update-intercept`, this.updateIntercept);
+    this.app.get(`${baseUrl}/intercepted`, this.getIntercept);
+    this.app.post(`${baseUrl}/delete-history`, this.deleteHistory);
+    this.app.get(`${baseUrl}/api-detail`, this.getApiDetail);
+    this.app.post(`${baseUrl}/api-save`, this.apiSave);
   }
 }
--- packages/interceptor/server/utils/file-util.ts
+++ packages/interceptor/server/utils/file-util.ts
@@ -1,9 +1,18 @@
+import { DbUtil } from "./db-util";
 import {
-  stat, readJSON, writeJSON, mkdir, readJSONSync, readJson, readJsonSync, writeJSONSync
-} from 'fs-extra';
-import { AxiosRequestConfig } from 'axios';
-import path from 'path';
-import { RequestUtil } from './request-util';
+  stat,
+  readJSON,
+  writeJSON,
+  mkdir,
+  readJSONSync,
+  readJson,
+  readJsonSync,
+  writeJSONSync,
+  removeSync
+} from "fs-extra";
+import { AxiosRequestConfig } from "axios";
+import path from "path";
+import { RequestUtil } from "./request-util";
 
 /**
 
@@ -29,8 +38,8 @@ export interface ProxyConfig {
 }
 
 export const defaultConfig: ProxyConfig = {
-  target: 'http://localhost:4000',
-  workDir: '.api-proxy',
+  target: "http://localhost:4000",
+  workDir: ".api-proxy",
   workPort: 10011
 };
 
@@ -39,15 +48,15 @@ export class FileUtil {
 
   private static config: ProxyConfig;
 
-  public static getFileName (url: string): string {
+  public static getFileName(url: string): string {
     // const str = url.split('?')[0];
     // return str.replace(/[\\/?:]/g, '-');
     return encodeURIComponent(url);
   }
 
-  public static loadConfig (): ProxyConfig {
+  public static loadConfig(): ProxyConfig {
     this.cwd = process.cwd();
-    const configFilePath = path.resolve(this.cwd, 'api-proxy.json');
+    const configFilePath = path.resolve(this.cwd, "api-proxy.json");
     try {
       const config = readJSONSync(configFilePath);
       return {
@@ -61,19 +70,45 @@ export class FileUtil {
     }
   }
 
-  public static async addRequestLog (path: string, req: AxiosRequestConfig, res: StoredRequest): Promise<void> {
+  public static async addRequestLog(
+    path: string,
+    req: AxiosRequestConfig,
+    res: StoredRequest
+  ): Promise<void> {
     const key = RequestUtil.getUniqueKeyFromRequest(req);
     try {
       const json = await readJSON(path);
       if (json) {
-        writeJSON(path, { ...json, [key]: {request: {params: req.params, body: req.data}, response: res} }, { spaces: 2 });
+        writeJSON(
+          path,
+          {
+            ...json,
+            [key]: {
+              request: { params: req.params, body: req.data },
+              response: res
+            }
+          },
+          { spaces: 2 }
+        );
       }
     } catch (error) {
-      writeJSON(path, { [key]: {request: {params: req.params, body: req.data}, response: res} }, { spaces: 2 });
+      writeJSON(
+        path,
+        {
+          [key]: {
+            request: { params: req.params, body: req.data },
+            response: res
+          }
+        },
+        { spaces: 2 }
+      );
     }
   }
 
-  public static async getRequestLog (path: string, req: AxiosRequestConfig): Promise<any> {
+  public static async getRequestLog(
+    path: string,
+    req: AxiosRequestConfig
+  ): Promise<any> {
     const key = RequestUtil.getUniqueKeyFromRequest(req);
     try {
       const json = await readJSON(path);
@@ -85,15 +120,63 @@ export class FileUtil {
     }
   }
 
-  public static getFilePath (url: string): string {
+  public static getFilePath(url: string): string {
     const fileName = this.getFileName(url);
-    return path.resolve(this.cwd, `${this.config.workDir}/history/${fileName}.json`);
+    return path.resolve(
+      this.cwd,
+      `${this.config.workDir}/history/${fileName}.json`
+    );
   }
 
-  public static async initFolder (): Promise<void> {
+  public static getOneHistory(api: string) {
+    const path = this.getFilePath(api);
+    return readJsonSync(path);
+  }
+
+  public static deleteOneLog(api: string) {
+    const path = this.getFilePath(api);
+    removeSync(path);
+    const db = DbUtil.getDb();
+    const { apiList = [] as string[], interceptList = [] as string[] } = db as {
+      apiList: string[];
+      interceptList: string[];
+    };
+    const apiIndex = apiList.indexOf(api);
+    const intepIndex = interceptList.indexOf(api);
+    if (apiIndex > -1) apiList.splice(apiIndex, 1);
+    if (intepIndex > -1) interceptList.splice(intepIndex, 1);
+    DbUtil.set("apiList", apiList);
+    DbUtil.set("interceptList", interceptList);
+  }
+
+  public static async updateRequestLog(data: any) {
+    const { path, key, response } = data;
+    try {
+      const filePath = this.getFilePath(path);
+      const json = await readJSON(filePath);
+      if (json) {
+        const target = json[key];
+        await writeJSON(
+          filePath,
+          {
+            ...json,
+            [key]: {
+              ...target,
+              response,
+            }
+          },
+          { spaces: 2 }
+        );
+      }
+    } catch (error) {
+      throw new Error("failed");
+    }
+  }
+
+  public static async initFolder(): Promise<void> {
     const config = (this.config = this.loadConfig());
     const rootDir = path.resolve(process.cwd(), config.workDir);
-    const historyDir = path.resolve(rootDir, 'history');
+    const historyDir = path.resolve(rootDir, "history");
     try {
       const fldStat = await stat(rootDir);
       if (!fldStat.isDirectory()) {
@@ -110,19 +193,25 @@ export class FileUtil {
     } catch (error) {
       await mkdir(historyDir);
     }
-    const settingFile = path.resolve(rootDir, 'settings.json');
+    const settingFile = path.resolve(rootDir, "settings.json");
     try {
       const json = await readJson(settingFile);
-    } catch(e) {
+    } catch (e) {
       writeJSON(settingFile, {});
     }
   }
 
   public static getSettings() {
-    return readJsonSync(path.resolve(this.cwd, this.config.workDir, 'settings.json'));
+    return readJsonSync(
+      path.resolve(this.cwd, this.config.workDir, "settings.json")
+    );
   }
 
   public static setSettings(json: any) {
-    return writeJSONSync(path.resolve(this.cwd, this.config.workDir, 'settings.json'), json, {spaces: 2});
+    return writeJSONSync(
+      path.resolve(this.cwd, this.config.workDir, "settings.json"),
+      json,
+      { spaces: 2 }
+    );
   }
 }
