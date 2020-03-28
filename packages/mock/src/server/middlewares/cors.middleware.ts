5c1624f84822e55dc05d819ce15d7a9810eee054
shihao wang <shihao.wang@hand-china.com>
2020-03-28 15:47:11 +0800

    fix: lint error

--- packages/mock/src/server/middlewares/cors.middleware.ts
+++ packages/mock/src/server/middlewares/cors.middleware.ts
@@ -1,9 +1,12 @@
 export function applyCors (req, res, next): void {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type,Content-Length, Authorization, Accept,X-Requested-With'
+  );
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
   res.header('X-Powered-By', ' 3.2.1');
-  if (req.method == 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     /* 让options请求快速返回 */
     res.status(200);
     res.send();
