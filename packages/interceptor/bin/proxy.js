88a1326010a4bb05a31aadf664c92f8bbeface2c
shihao wang <shihao.wang@hand-china.com>
2020-03-29 17:25:41 +0800

    feat: 新加生成默认配置选项

--- packages/interceptor/bin/proxy.js
+++ packages/interceptor/bin/proxy.js
@@ -2,15 +2,30 @@
 
 var program = require('commander');
 var startIntercept = require('../lib/main').default;
+const fs = require('fs');
+const path = require('path');
 
-program
+  program
   .version(require('../package.json').version)
-  .usage('<command> [options]')
+  .usage('[options]')
 
   program
     .option('-p --port <port>', 'mock服务运行的端口')
+    .option('-i --init', "创建一个默认的配置文件。")
   
   program.parse(process.argv);
+
+  if (program.init) {
+    const cwd = process.cwd();
+    try {
+      fs.readFileSync(path.resolve(cwd, '.smockrc.js'));
+      console.log('配置文件已存在，跳过创建。')
+    } catch (error) {
+      fs.createReadStream(path.resolve(__dirname, 'default-config.js'))
+        .pipe(fs.createWriteStream(path.resolve(cwd, '.smockrc.js')));
+      console.log('已创建配置文件。')
+    }
+  }
 
   let port = program.port;
   if (port) {
