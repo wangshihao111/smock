322ba6fdffea666e70e3db8750d77928b629c222
王世豪 <shihao.wang@hand-china.com>
2020-03-30 09:23:30 +0800

    fix: 更正编辑器显示

--- packages/interceptor/src/pages/ContentEdit/index.tsx
+++ packages/interceptor/src/pages/ContentEdit/index.tsx
@@ -1,16 +1,27 @@
-import React, { FC, useState, useEffect, useCallback, useLayoutEffect } from 'react'
-import { RouteChildrenProps } from 'react-router-dom'
+import React, {
+  FC,
+  useState,
+  useEffect,
+  useCallback,
+  useLayoutEffect,
+  ReactElement
+} from 'react';
+import { RouteChildrenProps } from 'react-router-dom';
 import { notification, Tabs, Button, Modal, Form, InputNumber } from 'antd';
-import { SyncOutlined, SaveOutlined, ArrowLeftOutlined } from '@ant-design/icons';
+import {
+  SyncOutlined,
+  SaveOutlined,
+  ArrowLeftOutlined
+} from '@ant-design/icons';
 import { AxiosError } from 'axios';
-import 'ace-builds'
+import 'ace-builds';
 import AceEditor from 'react-ace';
-import 'ace-builds/webpack-resolver'
-import "ace-builds/src-noconflict/mode-html";
-import "ace-builds/src-noconflict/mode-json";
-import "ace-builds/src-noconflict/theme-tomorrow";
+import 'ace-builds/webpack-resolver';
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-tomorrow';
 
-import './index.scss'
+import './index.scss';
 import { getApiDetail, saveApiData } from '../../service/mock.service';
 import HeadersEdit from './HeadersEdit';
 
@@ -22,30 +33,31 @@ interface HistoryItem {
     params: any;
     body: any;
   };
-  response: any
-};
+  response: any;
+}
 
-function parseResponseData(data: any): HistoryItem[] {
+function parseResponseData (data: any): HistoryItem[] {
   const result: HistoryItem[] = [];
   for (const key in data) {
     result.push({
       key,
       request: data[key].request,
       response: data[key].response
-    })
+    });
   }
   return result;
 }
 
 // 正在编辑的头信息，中间变量
-let editHeaders = {}
+let editHeaders = {};
 
-function getModeByHeader(headers = {} as any): string {
-  const contentType = headers['content-type'] || 'application/json';
-  return contentType.split('/')[1] || 'json'
-}
+// function getModeByHeader (headers = {} as any): string {
+//   const contentType = headers['content-type'] || 'application/json';
+//   console.log(contentType.split('/')[1]);
+//   return contentType.split('/')[1] || 'json';
+// }
 
-const ContentEdit: FC<RouteChildrenProps> = (props) => {
+const ContentEdit: FC<RouteChildrenProps> = props => {
   const path = decodeURIComponent((props.match?.params as any).api);
   const [form, setForm] = useState<string>('');
   const [editKey, setEditKey] = useState<string>('');
@@ -53,42 +65,47 @@ const ContentEdit: FC<RouteChildrenProps> = (props) => {
   const [detail, setDetail] = useState<HistoryItem[]>([]);
   const [editStatusNum, setEditStatusNum] = useState<number>(0);
   // const [showHeaderEdit, setShowHeaderEdit] = useState<boolean>(true);
-  const fetchDetail = useCallback((saved: boolean=false) => {
-    getApiDetail(path).then(res => {
-      const data = parseResponseData(res.data);
-      if (!saved) {
-        setForm(data[0].response.data);
-        setEditKey(data[0].key);
-        editHeaders = data[0].response.headers
-        setEditStatusNum(data[0].response.status);
-      }
-      setDetail(data);
-    }).catch((e: AxiosError) => {
-      let message = '';
-      if (e.response && e.response.data) {
-        message = e.response.data.message;
-      } else {
-        message = '获取失败，请重试';
-      }
-      notification.error({
-        message,
-      })
-    })
-  }, [path])
+  const fetchDetail = useCallback(
+    (saved = false) => {
+      getApiDetail(path)
+        .then(res => {
+          const data = parseResponseData(res.data);
+          if (!saved) {
+            setForm(data[0].response.data);
+            setEditKey(data[0].key);
+            editHeaders = data[0].response.headers;
+            setEditStatusNum(data[0].response.status);
+          }
+          setDetail(data);
+        })
+        .catch((e: AxiosError) => {
+          let message = '';
+          if (e.response && e.response.data) {
+            message = e.response.data.message;
+          } else {
+            message = '获取失败，请重试';
+          }
+          notification.error({
+            message
+          });
+        });
+    },
+    [path]
+  );
   useEffect(() => {
     fetchDetail();
-  }, [fetchDetail])
-  const handleContentChange = (value: string, key: string) => {
+  }, [fetchDetail]);
+  const handleContentChange = (value: string): void => {
     setForm(value);
-  }
-  const handleTabChange = (key: string) => {
+  };
+  const handleTabChange = (key: string): void => {
     const target = detail.find(v => v.key === key);
     setForm(target?.response.data);
     setEditKey(key);
     editHeaders = target?.response.headers;
     setEditStatusNum(target?.response.status);
-  }
-  const handleAction = (type: string) => () => {
+  };
+  const handleAction = (type: string) => (): void => {
     const target = detail.find(v => v.key === editKey);
     if (type === 'reset') {
       if (form !== target?.response.data) {
@@ -112,114 +129,139 @@ const ContentEdit: FC<RouteChildrenProps> = (props) => {
             ...target?.response,
             status: editStatusNum,
             headers: editHeaders,
-            data,
+            data
           }
-        }).then(res => {
-          fetchDetail(true)
-          notification.success({
-            message: '保存成功。'
+        })
+          .then(res => {
+            fetchDetail(true);
+            notification.success({
+              message: '保存成功。'
+            });
           })
-        }).catch((e: AxiosError) => {
-          let message = '';
-          if (e.response && e.response.data) {
-            message = e.response.data.message;
-          } else {
-            message = '保存失败。';
-          }
-          notification.error({
-            message,
+          .catch((e: AxiosError) => {
+            let message = '';
+            if (e.response && e.response.data) {
+              message = e.response.data.message;
+            } else {
+              message = '保存失败。';
+            }
+            notification.error({
+              message
+            });
           });
-        });
       } catch (error) {
         notification.error({
-          message: "格式化失败，请检查内容格式！"
-        })
+          message: '格式化失败，请检查内容格式！'
+        });
       }
     }
-  }
+  };
 
-  const handleHeadersChange = (values: any) => {
+  const handleHeadersChange = (values: any): void => {
     editHeaders = values;
-  }
-  function fitEditorHeight() {
+  };
+  function fitEditorHeight (): void {
     const height = window.innerHeight;
-    let editorHeight = height - 60 - 16 - 8 - 46 - 42;
+    const editorHeight = height - 60 - 16 - 8 - 46 - 42;
     setEh(editorHeight);
   }
   useLayoutEffect(() => {
     fitEditorHeight();
     window.addEventListener('resize', fitEditorHeight);
-    return () => {
+    return (): void => {
       window.removeEventListener('resize', fitEditorHeight);
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    };
   }, []);
-  const renderTab = (request: any) => {
+  const renderTab = (request: any): ReactElement => {
     return (
       <div className="history-tab-item-tab">
-        <pre>
-          {request}
-        </pre>
+        <pre>{request}</pre>
       </div>
-    )
-  }
+    );
+  };
   return (
     <div className="page-edit">
       <header className="page-edit-header">
-        <div className="back-arrow" onClick={() => props.history.push('/home')}>
+        <div className="back-arrow" onClick={(): void => props.history.push('/home')}>
           <ArrowLeftOutlined />
         </div>
         <h3 className="page-edit-header-title">接口编辑</h3>
         <div className="operation-wrap">
-          <Button type="link" onClick={() => props.history.push('/home')}>返回上一页</Button>
-          <Button type="link" icon={<SyncOutlined />} onClick={handleAction('reset')}>重置为初始值</Button>
-          <Button type="link" icon={<SaveOutlined />} onClick={handleAction('save')}>保存</Button>
+          <Button type="link" onClick={(): void => props.history.push('/home')}>
+            返回上一页
+          </Button>
+          <Button
+            type="link"
+            icon={<SyncOutlined />}
+            onClick={handleAction('reset')}
+          >
+            重置为初始值
+          </Button>
+          <Button
+            type="link"
+            icon={<SaveOutlined />}
+            onClick={handleAction('save')}
+          >
+            保存
+          </Button>
         </div>
       </header>
       <main className="page-edit-content">
         <Tabs tabPosition="left" onChange={handleTabChange}>
-          {
-            detail.map(({key, request, response}) => (
-              <TabPane key={key} tab={renderTab(request)}>
-                <div className="history-tab-content">
-                  <div className="content-edit">
-                    <header className="content-edit-title">
-                      响应体编辑
-                      <Form.Item label="响应状态码">
-                        <InputNumber size="small" value={editStatusNum} onChange={(e: number | undefined) => {setEditStatusNum(e || editStatusNum);}} />
-                      </Form.Item>
-                    </header>
-                    <AceEditor
-                      style={{width: '100%', height: eh, boxShadow: '-1px -1px 5px 0 #ababab'}}
-                      placeholder="Placeholder Text"
-                      mode={getModeByHeader(response.headers)}
-                      theme="tomorrow"
-                      onChange={v => handleContentChange(v, key)}
-                      fontSize={14}
-                      showPrintMargin={true}
-                      showGutter={true}
-                      highlightActiveLine={true}
-                      value={form}
-                      setOptions={{
-                        useWorker: false,
-                        enableBasicAutocompletion: false,
-                        enableSnippets: false,
-                        showLineNumbers: true,
-                        tabSize: 2,
-                      }}
-                    />
-                  </div>
-                  {
-                    <HeadersEdit style={{height: eh}} headers={response.headers} onChange={handleHeadersChange} />
-                  }
+          {detail.map(({ key, request, response }) => (
+            <TabPane key={key} tab={renderTab(request)}>
+              <div className="history-tab-content">
+                <div className="content-edit">
+                  <header className="content-edit-title">
+                    响应体编辑
+                    <Form.Item label="响应状态码">
+                      <InputNumber
+                        size="small"
+                        value={editStatusNum}
+                        onChange={(e: number | undefined): void => {
+                          setEditStatusNum(e || editStatusNum);
+                        }}
+                      />
+                    </Form.Item>
+                  </header>
+                  <AceEditor
+                    style={{
+                      width: '100%',
+                      height: eh,
+                      boxShadow: '-1px -1px 5px 0 #ababab'
+                    }}
+                    placeholder="Placeholder Text"
+                    mode="json"
+                    theme="tomorrow"
+                    onChange={(v): void => handleContentChange(v)}
+                    fontSize={14}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value={form}
+                    setOptions={{
+                      useWorker: false,
+                      enableBasicAutocompletion: false,
+                      enableSnippets: false,
+                      showLineNumbers: true,
+                      tabSize: 2
+                    }}
+                  />
                 </div>
-              </TabPane>
-            ))
-          }
+                {
+                  <HeadersEdit
+                    style={{ height: eh }}
+                    headers={response.headers}
+                    onChange={handleHeadersChange}
+                  />
+                }
+              </div>
+            </TabPane>
+          ))}
         </Tabs>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default ContentEdit
+export default ContentEdit;
