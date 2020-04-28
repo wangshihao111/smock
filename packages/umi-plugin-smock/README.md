5c4178020c8c92d478aacda172cb0fdc34f876a1
shihao wang <shihao.wang@hand-china.com>
2020-04-28 21:22:44 +0800

    feat: 加入环境变量支持

--- packages/umi-plugin-smock/README.md
+++ packages/umi-plugin-smock/README.md
@@ -10,5 +10,11 @@ npm i @smock/umi-plugin-smock
 yarn add @smock/umi-plugin-smock
 ```
 
+指定NO_MOCK环境变量可禁用mock
+例如:
+```bash
+export NO_MOCK=true
+```
+
 smock工具参考文档：
 [https://www.npmjs.com/package/@smock/mock](https://www.npmjs.com/package/@smock/mock)
--- packages/umi-plugin-smock/index.ts
+++ packages/umi-plugin-smock/index.ts
@@ -1,7 +1,8 @@
 import { createExpressMiddleware } from "@smock/mock/lib/server/server";
 
 module.exports = (api) => {
-  if (api.env === 'development') {
+  
+  if (api.env === 'development' && !process.env.NO_MOCK) {
     api.addBeforeMiddewares(() => {
       return createExpressMiddleware(api.getPort());
     });
