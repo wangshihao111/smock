4f724e1a1e11876bbbe026eb06d4f3af79edbb2a
shihao wang <shihao.wang@hand-china.com>
2020-05-14 15:35:42 +0800

    fix: 拦截器重定向

--- packages/interceptor/server/utils/request-util.ts
+++ packages/interceptor/server/utils/request-util.ts
@@ -113,7 +113,7 @@ export class RequestUtil {
       if (key === "location") {
         const loc = value.replace(
           this.ctx.config.target,
-          `http://localhost:${this.ctx.config.workPort}/${apiPrefix}`
+          `http://localhost:${this.ctx.config.workPort}${apiPrefix}`
         );
         // console.log('原始的__:', headers.location);
         // console.log('替换过的__:', loc)
