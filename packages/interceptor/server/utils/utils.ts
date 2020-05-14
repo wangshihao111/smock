af6797d57b65fa5b9fac2f211ba372c141a05552
shihao wang <shihao.wang@hand-china.com>
2020-05-14 15:23:10 +0800

    fix: 创建默认配置文件

--- packages/interceptor/server/utils/utils.ts
+++ packages/interceptor/server/utils/utils.ts
@@ -12,7 +12,7 @@ export function initConfigFile() {
   const defaultFile = path.resolve(__dirname, "../../bin/default-config.js");
   if (!fs.existsSync(file)) {
     const content = fs.readFileSync(defaultFile, "utf8");
-    fs.writeFileSync(file, content, "urf8");
+    fs.writeFileSync(file, content, "utf8");
     console.log("默认配置文件创建成功。");
   }
 }
