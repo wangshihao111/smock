fd209d9a73ee822a3f13acfc2d3a589ec49d1fd8
shihao wang <shihao.wang@hand-china.com>
2020-05-14 15:04:43 +0800

    feat: 更改工具函数引入

--- packages/mock/src/server/server.ts
+++ packages/mock/src/server/server.ts
@@ -1,13 +1,14 @@
 import bodyParser from "body-parser"
-import express, { Application, Request, Response, request } from "express"
+import express, { Application, Request, Response } from "express"
 import { resolve } from "path"
 import { MockService } from "./mock/mock.service"
 import { ApiCorsService } from "./mock/api-cors.service"
 import { applyCors } from "./middlewares/cors.middleware"
 import chalk from "chalk"
+import { serveStatic } from "@smock/utils"
 
 export function runMock(app: Application, port: number, host: string): void {
-  app.use("/__doc", express.static(resolve(__dirname, "../../dist")))
+  app.use("/__doc", serveStatic(resolve(__dirname, "../../dist"), {}))
   const mockInstance = new MockService(app, port, host)
   const apiCorsService = new ApiCorsService(app)
   mockInstance.init()
@@ -28,10 +29,8 @@ export function createExpressMiddleware(port) {
   const instance = new MockService({} as any, port, "127.0.0.1")
   console.log(chalk.green(`mock服务运行在：http://127.0.0.1:${port}`))
   console.log(chalk.green(`你可以在浏览器打开此地址以查看文档：http://127.0.0.1:${port}/__doc__`))
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const serveStatic = require("../utils/serve-static")
   const apiCorsMiddleware = new ApiCorsService().createMiddleware()
-  const staticMiddleware = serveStatic(resolve(__dirname, "../../dist"))
+  const staticMiddleware = serveStatic(resolve(__dirname, "../../dist"), { basePath: "__doc__" })
   const mockMiddleware = instance.createMiddleware()
   return (request: Request, response: Response, next) => {
     const requestPath = request.path || ""
