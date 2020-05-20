36eb4c6ab73df64351a9b05b26fc5bbf6dd1101b
shihao wang <shihao.wang@hand-china.com>
2020-05-20 21:02:48 +0800

    feat: 修改展示

--- packages/interceptor/src/pages/About/index.tsx
+++ packages/interceptor/src/pages/About/index.tsx
@@ -7,7 +7,7 @@ const About: FC = () => {
       <h2>关于我们</h2>
       <p>
         如果使用中发现问题，欢迎联系
-        <a href="email: 913003120@qq.com">913003120@qq.com</a>
+        {/* <a href="email: 913003120@qq.com">913003120@qq.com</a> */}
       </p>
       <p>或去我们的仓库提issues</p>
       <p>
