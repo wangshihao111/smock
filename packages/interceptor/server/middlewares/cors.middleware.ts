8a9678deeb86e6c1318252b3abd02199c2c7d7c6
shihao wang <shihao.wang@hand-china.com>
2020-05-22 09:19:27 +0800

    fix: 部分跨域问题

--- packages/interceptor/server/middlewares/cors.middleware.ts
+++ packages/interceptor/server/middlewares/cors.middleware.ts
@@ -1,16 +1,20 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-export function applyCors (req: Request, res: Response, next: Function): void {
+export function applyCors(req: Request, res: Response, next: Function): void {
   const { headers } = req;
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Origin', headers.origin || `http://${headers.host}`);
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, cache-control, Content-Length, Authorization, Accept, X-Requested-With, origin, pragma'
+    "Access-Control-Allow-Origin",
+    headers.origin || `http://${headers.host}`
   );
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-  res.header('X-Powered-By', ' 3.2.1');
-  if (req.method === 'OPTIONS') {
+
+  const headerKeys =
+    Object.keys(headers || {}).join(",") +
+    ",cache-control, Content-Length, Authorization, Accept, X-Requested-With, origin, pragma, h-menu-id, content-type";
+  res.header("Access-Control-Allow-Headers", headerKeys);
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  if (req.method === "OPTIONS") {
     /* 让options请求快速返回 */
     res.status(200);
     res.send();
@@ -19,17 +23,17 @@ export function applyCors (req: Request, res: Response, next: Function): void {
   }
 }
 
-export function toCors (req: Request, res: Response) {
+export function toCors(req: Request, res: Response) {
   const { headers } = req;
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Origin', headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", headers.origin);
   res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, cache-control, Content-Length, Authorization, Accept, X-Requested-With, origin, pragma'
+    "Access-Control-Allow-Headers",
+    "Content-Type, cache-control, Content-Length, Authorization, Accept, X-Requested-With, origin, pragma"
   );
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-  res.header('X-Powered-By', ' 3.2.1');
-  if (req.method === 'OPTIONS') {
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  if (req.method === "OPTIONS") {
     res.status(200);
     res.send();
   }
