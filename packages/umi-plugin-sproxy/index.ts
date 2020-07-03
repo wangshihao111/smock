d8f8a9a8f10d106e93e3c8094fb7c9aee2d20e6d
shihao wang <shihao.wang@hand-china.com>
2020-07-03 14:22:33 +0800

    feat: 更改禁用环境变量

--- packages/umi-plugin-sproxy/index.ts
+++ packages/umi-plugin-sproxy/index.ts
@@ -2,7 +2,7 @@
 import startProxy from '@smock/interceptor/lib/main';
 
 module.exports = (api) => {
-  if (api.env === "development" && !process.env.NO_INTERCEPTOR) {
+  if (api.env === "development" && !process.env.NO_INTERCEPTOR && !process.env.NO_SPROXY) {
     if (!(global as any)['__smock/proxy']) {
       startProxy();
       (global as any)['__smock/proxy'] = true;
