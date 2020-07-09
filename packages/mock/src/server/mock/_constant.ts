da21f2261ad6c2339d57e9a8d14294d5b9169a79
shihao wang <shihao.wang@hand-china.com>
2020-07-09 21:27:24 +0800

    delete: 删除冗余配置

--- packages/mock/src/server/mock/_constant.ts
+++ packages/mock/src/server/mock/_constant.ts
@@ -1,9 +1,2 @@
-import { IOptions } from "glob"
-
 export const mockDir = "_smock"
 export const mockFilePrefix = "_smock"
-
-export const globOptions: IOptions = {
-  ignore: ["**/node_modules/**"],
-  root: process.cwd(),
-}
