bee07a02871006e97535d021264a8bef5e3cc5fe
shihao wang <shihao.wang@hand-china.com>
2020-12-29 15:46:02 +0800

    feat: 添加虹珊瑚控制器

--- packages/umi-plugin-sproxy/index.ts
+++ packages/umi-plugin-sproxy/index.ts
@@ -1,5 +1,6 @@
 // import { IApi } from "@umijs/types";
 import startProxy from '@smock/interceptor/lib/main';
+import {resolve} from 'path';
 
 module.exports = (api) => {
   const {NO_INTERCEPTOR, NO_SPROXY} = process.env || {};
@@ -9,8 +10,29 @@ module.exports = (api) => {
   }
   if (api.env === "development") {
     if (!(global as any)['__smock/proxy']) {
-      startProxy();
+      try {
+        startProxy();
+      } catch (error) {
+        console.log(error);
+      }
       (global as any)['__smock/proxy'] = true;
+    }
+    if (typeof api.onUISocket === 'function') {
+      api.onUISocket(async ({ action, failure, success, send }) => {
+        if (action.type === 'smock/sproxy-port') {
+          const cwd = process.cwd();
+          let port = 10011;
+          const paths = [resolve(cwd, '.smockrc.js'), resolve(cwd, '../../smockrc.js')];
+          for (let i = 0; i < paths.length; i++) {
+            const p = paths[i];
+            try {
+              port = require(p).workPort;
+              break;
+            } catch (error) {}
+          }
+          success(port);
+        }
+      })
     }
   }
 };
---
+++ packages/umi-plugin-sproxy/plugin-config.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+  createClientController() {
+    return {
+      title: "拦截工具",
+      icon: "@smock/umi-plugin-sproxy/icon.svg",
+      controller: async ({ open }) => {
+        const callRemote = (window as any).umi_callRemote
+        let port = 0
+        if (typeof callRemote === 'function') {
+          port = (await callRemote({ type: 'global/smock/sproxy-port' })) || 10011;
+        }
+        if (open) {
+          window.open(`http://localhost:${port}`);
+        }
+      },
+    };
+  },
+
+  // node controller
+  createNodeController() {
+    const { __global_handler } = global as any;
+    if (__global_handler && __global_handler.onSocket) {
+      __global_handler.onSocket(({ action, success }) => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const { resolve } = require('path');
+        if (action.type === 'global/smock/sproxy-port') {
+          const cwd = process.cwd();
+          let port = 10011;
+          const paths = [resolve(cwd, '.smockrc.js'), resolve(cwd, '../../smockrc.js')];
+          for (let i = 0; i < paths.length; i++) {
+            const p = paths[i];
+            try {
+              port = require(p).workPort;
+              break;
+            } catch (error) { }
+          }
+          success(port);
+        }
+      })
+    }
+
+    return function ({ enable = true } = {}) {
+      Object.defineProperties(process.env, {
+        NO_SPROXY: {
+          get() {
+            return enable ? "" : "true";
+          },
+          // 这里设定的优先级最高
+          set() {
+            this.value = enable ? "" : "true";
+          },
+        },
+      });
+    };
+  },
+};
