d8bada754b84e1d648de974f8c85c7ba4dc070ea
shihao wang <shihao.wang@hand-china.com>
2021-04-09 10:11:22 +0800

    fix: 启用逻辑

--- packages/umi-plugin-sproxy/index.ts
+++ packages/umi-plugin-sproxy/index.ts
@@ -1,28 +1,42 @@
 // import { IApi } from "@umijs/types";
-import startProxy from '@smock/interceptor/lib/main';
-import {resolve} from 'path';
+import startProxy from "@smock/interceptor/lib/main";
+import { resolve } from "path";
+
+const { argv } = process;
+
+function isHzeroCLIDev() {
+  return argv.some((str) => str.match("hzero-cli")) && argv.includes("start");
+}
+
+function isUmiCLIDev() {
+  return argv.some((str) => str.match("umi")) && argv.includes("dev");
+}
 
 module.exports = (api) => {
-  const {NO_INTERCEPTOR, NO_SPROXY} = process.env || {};
-  
-  if (NO_INTERCEPTOR === 'true' || NO_SPROXY=== 'true') {
+  const { NO_INTERCEPTOR, NO_SPROXY } = process.env || {};
+
+  if (NO_INTERCEPTOR === "true" || NO_SPROXY === "true") {
     return;
   }
-  if (api.env === "development") {
-    if (!(global as any)['__smock/proxy']) {
+  const isDev = isHzeroCLIDev() || isUmiCLIDev();
+  if (api.env === "development" && isDev) {
+    if (!(global as any)["__smock/proxy"]) {
       try {
         startProxy();
       } catch (error) {
         console.log(error);
       }
-      (global as any)['__smock/proxy'] = true;
+      (global as any)["__smock/proxy"] = true;
     }
-    if (typeof api.onUISocket === 'function') {
+    if (typeof api.onUISocket === "function") {
       api.onUISocket(async ({ action, failure, success, send }) => {
-        if (action.type === 'smock/sproxy-port') {
+        if (action.type === "smock/sproxy-port") {
           const cwd = process.cwd();
           let port = 10011;
-          const paths = [resolve(cwd, '.smockrc.js'), resolve(cwd, '../../smockrc.js')];
+          const paths = [
+            resolve(cwd, ".smockrc.js"),
+            resolve(cwd, "../../smockrc.js"),
+          ];
           for (let i = 0; i < paths.length; i++) {
             const p = paths[i];
             try {
@@ -32,7 +46,7 @@ module.exports = (api) => {
           }
           success(port);
         }
-      })
+      });
     }
   }
 };
